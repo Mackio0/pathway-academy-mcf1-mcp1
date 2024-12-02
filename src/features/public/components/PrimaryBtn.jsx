e511fa4a98ad1ae6a2aff89c903b3b85d0019244
Mackio0 <asiamaman000@gmail.com>
2024-12-02 21:40:09 +0630

    Revert "Merge branch 'dev' of https://github.com/Mackio0/pathway-academy-mcf1-mcp1 into dev"
    This reverts commit b94e17fc0a7d1d6cb2690ed83a5256aef221f02f, reversing
    changes made to b933cf6f82626eceb000963bc3a7990727084d97.

--- src/features/public/components/PrimaryBtn.jsx
+++ src/features/public/components/PrimaryBtn.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
-const PrimaryBtn = ({ className, children, onClick, isBlack = false }) => {
+const PrimaryBtn = ({
+  className,
+  children,
+  onClick,
+  isBlack = false,
+  type = "button",
+  isFontBold = false,
+}) => {
   return (
     <>
       <button
-        type="button"
+        type={type}
         onClick={onClick && onClick}
         className={
           isBlack
-            ? `text-white flex justify-center items-center border-2 border-heading hover:border-para3 bg-heading hover:bg-para3 focus:ring-2 focus:ring-primary-500 font-medium rounded text-sm dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 ${className}`
-            : `text-white flex justify-center items-center border-2 border-primary-500 hover:border-primary-700 bg-primary-500 hover:bg-primary-700 focus:ring-2 focus:ring-primary-300 font-medium rounded text-sm dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 ${className}`
+            ? `text-white flex justify-center items-center border-2 border-heading hover:border-para3 bg-heading hover:bg-para3 focus:ring-2 focus:ring-primary-500 rounded text-sm dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 ${
+                isFontBold ? "font-bold" : "font-medium"
+              } ${className}`
+            : `text-white flex justify-center items-center border-2 border-primary-500 hover:border-primary-700 bg-primary-500 hover:bg-primary-700 focus:ring-2 focus:ring-primary-300 rounded text-sm dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 ${
+                isFontBold ? "font-bold" : "font-medium"
+              } ${className}`
         }
       >
         {children}
